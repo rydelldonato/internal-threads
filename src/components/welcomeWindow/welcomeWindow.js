2de1942e369310e551cbd565f7c58f9f9fd82073
rydelldonato <rydell.donato@gmail.com>
2023-12-02 14:39:34 -0800

    added logo

--- src/components/welcomeWindow/welcomeWindow.js
+++ src/components/welcomeWindow/welcomeWindow.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import HeartButton from '../heartButton/heartButton'
-import './welcomeWindow.css'
+import React from "react";
+import HeartButton from "../heartButton/heartButton";
+import "./welcomeWindow.css";
 
 export default function WelcomeWindow() {
   return (
     <>
-    <div className='allContent'>
-        <div className='top'>
-            <div className='welcome'>
-              welcome
-            </div>
-            <div className='heartButton'>
-                <HeartButton/>
-            </div>
+      <div className="allContent">
+        <div className="top">
+          <div className="welcome">welcome</div>
+          <div className="heartButton">
+            <HeartButton />
+          </div>
         </div>
-        <div className='mainWindow'>
-          login button
+        <div className="logoDiv">
+          <img className="logo" alt="logo" src="./logo.jpeg"/> 
         </div>
-    </div>
+        <div className="mainWindow">login button</div>
+      </div>
     </>
-  )
+  );
 }
